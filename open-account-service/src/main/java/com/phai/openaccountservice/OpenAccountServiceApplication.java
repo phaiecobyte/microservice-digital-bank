086e3089894c361bdc63774e3eefe30100806b91
phaiecobyte <brave6823@gmail.com>
2025-06-05 16:42:16 +0700

    add open account service

---
+++ open-account-service/src/main/java/com/phai/openaccountservice/OpenAccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.phai.openaccountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenAccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenAccountServiceApplication.class, args);
+	}
+
+}
---
+++ open-account-service/src/test/java/com/phai/openaccountservice/OpenAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phai.openaccountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenAccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
